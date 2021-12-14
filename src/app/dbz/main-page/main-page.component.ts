e2f48c8094d319f45cd27b9f0fbc2e3216df330a
Edison <edison@ucrop.it>
2021-12-14 07:40:20 -0400

    feature: output between component father and son

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,42 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Personaje } from '../interface/dbz.interface';
 
-
-
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  
 })
-export class MainPageComponent{
-
-  personajes: Personaje[] = [{
-    nombre: 'Goku',
-    poder: 15000
-  },
-  {
-    nombre: 'Vegeta',
-    poder: 7500
-  }
-];
+export class MainPageComponent {
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000,
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500,
+    },
+  ];
 
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0
+    nombre: 'trunks',
+    poder: 8000,
+  };
+
+  agregarNuevoPersonaje(argumento: Personaje) {
+    this.personajes.push(argumento);
   }
-
-  agregar() {
-    if(this.nuevo.nombre.trim().length === 0){
-      return;
-    }
-
-    this.personajes.push(this.nuevo)
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
-  }
-
-
 }
--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,13 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Personaje } from '../interface/dbz.interface';
 
 @Component({
   selector: 'app-personajes',
   templateUrl: './personajes.component.html',
-  styleUrls: ['./personajes.component.css']
+  styleUrls: ['./personajes.component.css'],
 })
-export class PersonajesComponent  {
-
+export class PersonajesComponent {
   @Input() personajes: Personaje[] = [];
-
 }
